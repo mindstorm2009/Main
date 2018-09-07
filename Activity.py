a4e45ce4259381d21773e2145dc983ff9af110a9
mindstorm2009 <curl.nicholas@gmail.com>
2018-09-07 08:55:36 -0400

    Initial commit

---
+++ Activity.py
@@ -0,0 +1,23 @@
+"""
+Nicholas Curl
+"""
+
+import turtle
+
+
+def draw_square():
+    turtle.forward(50)
+    turtle.right(90)
+    turtle.forward(50)
+    turtle.right(90)
+    turtle.forward(50)
+    turtle.right(90)
+    turtle.forward(50)
+
+
+def main():
+    draw_square()
+    turtle.done()
+
+
+main()
