8f1429f3736ee54e8dea4b78cd37b9d5c4891fe2
mindstorm2009 <curl.nicholas@gmail.com>
2018-10-01 12:52:32 -0400

    Updated File

--- reverse.py
+++ reverse.py
@@ -2,12 +2,15 @@ def reverse_rec(a_string):
     if a_string == "":
         return ""
     else:
-        return a_string[0] + reverse_rec(a_string[1:])
+        gnirts = reverse_rec(a_string[1:]) + a_string[0]
+        return gnirts
+
 
 def main():
     a_string = input("Enter a string: ")
     gnirts_a = reverse_rec(a_string)
     print(gnirts_a)
+
 
 if __name__ == "__main__":
     main()
--- test_reverse.py
+++ test_reverse.py
@@ -1,5 +1,6 @@
 import reverse
 
+
 def test_reverse_rec(name, a_string, expected):
     result = reverse.reverse_rec(a_string)
     if result == expected:
@@ -7,17 +8,22 @@ def test_reverse_rec(name, a_string, expected):
     else:
         print(name, "failed; expected", expected, "got", result)
 
+
 def test_reverse_rec_empty():
     test_reverse_rec("empty", "", "")
+
 
 def test_reverse_rec_one():
     test_reverse_rec("one", "a", "a")
 
+
 def test_reverse_rec_three():
     test_reverse_rec("three", "abc", "cba")
 
+
 def test_reverse_rec_palindrome():
     test_reverse_rec("palindrome", "PalindromemordnilaP", "PalindromemordnilaP")
+
 
 def run_all_tests():
     test_reverse_rec_empty()
@@ -25,8 +31,6 @@ def run_all_tests():
     test_reverse_rec_three()
     test_reverse_rec_palindrome()
 
+
 if __name__ == "__main__":
     run_all_tests()
-
-
-    
