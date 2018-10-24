ac59cd4a398dd8a54a7920612f17b3b2de5f032e
mindstorm2009 <curl.nicholas@gmail.com>
2018-10-23 22:05:19 -0400

    updated file

--- activity.py
+++ activity.py
@@ -1,21 +1,38 @@
-def fib(n):
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    else:
-        return fib(n - 1) + fib(n - 2)
+from merge_sort import *
+from quick_sort import *
+from random import *
+from math import *
+from time import *
 
-def fibl(n):
-    result = 0
-    while n >= 2:
-        result = result + fib(n-1) + fib(n-2)
-        n = n-1
-        print(result)
+def create_random_list(n, limit):
+    L = []
+    for i in range(n):
+        L += [randint(0, limit)]
+    return L
+
+
+def isSorted(L):
+    index = 0
+    sorted = False
+    for i in range(len(L) - 1):
+        if L[i] > L[i + 1]:
+            return False
+    return True
+
+def calc_square_root():
+    start = time()
+    for i in range(10000000):
+        sqrt(i)
+    end = time()
+    return end - start
+
 def main():
-    n = 0
-    while n<40:
-        print(n, fib(n))
-        n= n+1
+    lst = create_random_list(5, 1000)
+    test_lst = lst[:]
+    test_lst.sort()
+    print(isSorted(lst))
+    print(isSorted(test_lst))
+    print(calc_square_root(), " seconds")
+
 
 main()
